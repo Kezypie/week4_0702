aa153b1901670723fbde0cc3e862db90f21d1472
Kezypie <wwwkrisna502@gmail.com>
2025-10-22 15:50:20 +0700

    add try

--- belajarvariabel.java
+++ belajarvariabel.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 public class belajarvariabel {
     public static void main(String[] args) {
         int nUmur;
-        Scanner ns = new Scanner(System.in);
-
-        System.out.println("masukkan umur");
-        nUmur = ns.nextInt();
-        System.out.println("umur anda adalah " + nUmur);
-        ns.close();
+        try (Scanner ns = new Scanner(System.in)) {
+            System.out.println("masukkan umur");
+            nUmur = ns.nextInt();
+            System.out.println("umur anda adalah " + nUmur);
+            ns.close();
+        }
     
     }
 }
---
+++ jawajawa.java
@@ -0,0 +1,5 @@
+public class jawajawa {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
