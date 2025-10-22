eb1c81d4c14af0b07d04f0f4a6d85c53d8f07344
Kezypie <wwwkrisna502@gmail.com>
2025-10-22 15:45:10 +0700

    output

--- belajarvariabel.java
+++ belajarvariabel.java
@@ -5,8 +5,11 @@ public class belajarvariabel {
     public static void main(String[] args) {
         int nUmur;
         Scanner ns = new Scanner(System.in);
-        
+
         System.out.println("masukkan umur");
         nUmur = ns.nextInt();
+        System.out.println("umur anda adalah " + nUmur);
+        ns.close();
+    
     }
 }
